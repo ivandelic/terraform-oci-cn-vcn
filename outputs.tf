2e3cdf6d4e838e1e0a99ce0a29f518d5f7d1d20a
Ivan Delic <ivan.delic@ivan.delic-mac>
2023-01-04 15:25:01 +0100

    Describing output variables.

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,9 @@
 output "vcn_id" {
+  description = "OCID of created VCN."
   value = oci_core_vcn.vcn.id
 }
 
 output "subnets" {
+  description = "Array of created subnets."
   value = oci_core_subnet.subnet
 }
